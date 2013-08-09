9886e4df6774a2eab6545354161ca163ab9f2599
Philip Salvaggio <philipsalvaggio@reefshark.local>
2013-08-09 09:31:19 -0400

    Input reader for the ENVI image file format.

---
+++ io/envi_image_reader.cpp
@@ -0,0 +1,342 @@
+// A file reader class for ENVI images.
+// Author: Philip Salvaggio
+
+#include "envi_image_reader.h"
+
+#include "io/envi_image_header.pb.h"
+#include "io/logging.h"
+#include "base/endian.h"
+#include "base/str_utils.h"
+
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+using mats::trim;
+
+namespace mats_io {
+
+EnviImageReader::EnviImageReader() {}
+
+bool EnviImageReader::Read(const string& image_filename,
+                           const string& header_filename,
+                           EnviImageHeader* hdr,
+                           vector<cv::Mat>* image) {
+  // Validate the input.
+  if (!hdr || !image) {
+    mainLog() << "Invalid header or image pointer" << endl;
+    return false;
+  }
+  image->clear();
+
+  // Read in the ENVI header file.
+  if (!ReadHeader(header_filename, hdr)) {
+    mainLog() << "Error: Invalid ENVI header file \"" << header_filename
+              << "\"" << endl;
+    return false;
+  }
+
+  if (!hdr->has_byte_order()) {
+    mainLog() << "Error: No byte order given in ENVI header file \""
+              << header_filename << "\"" << endl;
+    return false;
+  }
+
+  if (!hdr->has_data_type()) {
+    mainLog() << "Error: ENVI header \"" << header_filename
+              << "missing data type." << endl;
+    return false;
+  }
+  
+  int depth = 0;
+  int cv_type = GetOpenCVDType(hdr->data_type(), depth);
+  if (cv_type == -1) return false;
+
+  // Size of the data.
+  const int kSize = hdr->lines() * hdr->samples() * hdr->bands() * depth;
+
+  if (!hdr->has_bands() || !hdr->has_samples() || !hdr->has_lines() ||
+      hdr->bands() <= 0 || hdr->samples() <= 0 || hdr->lines() <= 0) {
+    mainLog() << "Error: Missing or invalid image dimensions in ENVI header "
+              << "file \"" << header_filename << "\"" << endl;
+    return false;
+  }
+
+  if (hdr->band_size() != hdr->bands()) {
+    mainLog() << "The number of bands in the ENVI header does not match up "
+              << "with the number of bands described in the \"band names\"/"
+              << "wavelengths/... sections." << endl;
+    return false;
+  }
+
+  // Allocate the memory for the bands;
+  for (int i = 0; i < hdr->bands(); i++) {
+    image->push_back(cv::Mat(hdr->lines(), hdr->samples(), cv_type));
+  }
+
+  // Open the image file.
+  FILE* img_file = fopen(image_filename.c_str(), "rb");
+  if (!img_file) {
+    mainLog() << "Can not open image file \"" << image_filename
+              << "\"" << endl;
+    return false;
+  }
+
+  if (fseek(img_file, hdr->header_offset(), SEEK_SET) != 0) {
+    mainLog() << "The header offset in the ENVI header \"" << header_filename
+              << "\" was invalid." << endl;
+    return false;
+  }
+
+  unsigned char* raw_data = new unsigned char[kSize];
+  if (fread(raw_data, 1, kSize, img_file) != kSize) {
+    mainLog() << "Not enough data in the image file." << endl;
+    delete[] raw_data;
+    return false;
+  }
+  fclose(img_file);
+
+  switch (cv_type) {
+    case CV_8UC1: RearrangeImageData(raw_data, hdr, image); break;
+    case CV_16SC1: RearrangeImageData((int16_t*)raw_data, hdr, image); break;
+    case CV_32SC1: RearrangeImageData((int32_t*)raw_data, hdr, image); break;
+    case CV_32FC1: RearrangeImageData((float*)raw_data, hdr, image); break;
+    case CV_64FC1: RearrangeImageData((double*)raw_data, hdr, image); break;
+    case CV_16UC1: RearrangeImageData((uint16_t*)raw_data, hdr, image); break;
+  }
+
+  delete[] raw_data;
+
+  return true;
+}
+
+bool EnviImageReader::ReadHeader(const string& header_filename,
+                                 EnviImageHeader* hdr) {
+  ifstream ifs(header_filename.c_str());
+
+  if (!ifs.is_open()) {
+    mainLog() << "Could not open file: " << header_filename << endl;
+    return false;
+  }
+  
+  string line, multiline;
+
+  getline(ifs, line);
+  if (line != "ENVI") {
+    mainLog() << "First line of ENVI header file should be \"ENVI\"" << endl;
+    return false;
+  }
+
+  getline(ifs, line);
+  multiline = line;
+
+  int line_num = 3;
+  while (!ifs.eof()) {
+    getline(ifs, line);
+
+    if (line.find_first_of("=") != string::npos) {
+      if (!ParseHeaderLine(multiline, hdr)) {
+        mainLog() << "Invalid ENVI header line encountered at line "
+                  << line_num << endl;
+        return false;
+      }
+           
+      multiline = "";
+    }
+
+    multiline.append(trim(line));
+    line_num++;
+  }
+
+  if (!ParseHeaderLine(multiline, hdr)) {
+    mainLog() << "Invalid ENVI header line encountered at line "
+              << line_num << endl;
+    return false;
+  }
+
+  return true;
+}
+
+bool EnviImageReader::ParseHeaderLine(const string& line,
+                                      EnviImageHeader* hdr) {
+  int equal_index = line.find_first_of("=");
+  if (equal_index == string::npos) {
+    mainLog() << "No \"=\" found in the line." << endl;
+    return false;
+  }
+
+  string tag_name = line.substr(0, equal_index);
+  trim(tag_name);
+  std::transform(tag_name.begin(), tag_name.end(), tag_name.begin(), ::tolower);
+  string value = line.substr(equal_index + 1);
+  trim(value);
+
+  if (tag_name == "description") {
+    hdr->set_description(value);
+  } else if (tag_name == "samples") {
+    hdr->set_samples(atoi(value.c_str()));
+  } else if (tag_name == "lines") {
+    hdr->set_lines(atoi(value.c_str()));
+  } else if (tag_name == "bands") {
+    hdr->set_bands(atoi(value.c_str()));
+  } else if (tag_name == "header offset") {
+    hdr->set_header_offset(atoi(value.c_str()));
+  } else if (tag_name == "file type") {
+    hdr->set_file_type(value);
+  } else if (tag_name == "data type") {
+    hdr->set_data_type(atoi(value.c_str()));
+  } else if (tag_name == "interleave") {
+    std::transform(value.begin(), value.end(), value.begin(), ::tolower);
+    if (value == "bip") {
+      hdr->set_interleave_type(EnviImageHeader::BIP);
+    } else if (value == "bil") {
+      hdr->set_interleave_type(EnviImageHeader::BIL);
+    } else if (value == "bsq") {
+      hdr->set_interleave_type(EnviImageHeader::BSQ);
+    } else {
+      mainLog() << "Invalid interleave type given in ENVI header." << endl;
+      return false;
+    }
+  } else if (tag_name == "sensor type") {
+    hdr->set_sensor_type(value);
+  } else if (tag_name == "byte order") {
+    hdr->set_byte_order(atoi(value.c_str()));
+  } else if (tag_name == "x-start") {
+    hdr->set_start_x(atoi(value.c_str()));
+  } else if (tag_name == "y-start") {
+    hdr->set_start_y(atoi(value.c_str()));
+  } else if (tag_name == "map info") {
+    hdr->set_map_info(value);
+  } else if (tag_name == "projection info") {
+    hdr->set_projection_info(value);
+  } else if (tag_name == "default bands") {
+    hdr->set_default_bands(value);
+  } else if (tag_name == "z plot range") {
+    hdr->set_z_plot_range(value);
+  } else if (tag_name == "z plot average") {
+    hdr->set_z_plot_average(value);
+  } else if (tag_name == "z plot titles") {
+    hdr->set_z_plot_titles(value);
+  } else if (tag_name == "pixel size") {
+    hdr->set_pixel_size(value.c_str());
+  } else if (tag_name == "default stretch") {
+    hdr->set_default_stretch(value);
+  } else if (tag_name == "wavelength units") {
+    hdr->set_wavelength_units(value);
+  } else if (tag_name == "band names") {
+    vector<string> band_names;
+    mats::explode(value, ',', &band_names);
+
+    if (hdr->band_size() > 0 && hdr->band_size() != band_names.size()) {
+      mainLog() << "Wavelengths, FWHM, band names and bad bands list all "
+                << "need to be the same length." << endl;
+      return false;
+    }
+
+    if (hdr->band_size() == 0) {
+      for (size_t i = 0; i < band_names.size(); i++) {
+        EnviBand* band = hdr->add_band();
+        band->set_name(band_names[i]);
+      }
+    } else {
+      for (size_t i = 0; i < band_names.size(); i++) {
+        hdr->mutable_band(i)->set_name(band_names[i]);
+      }
+    }
+  } else if (tag_name == "wavelength") {
+    vector<string> wavelengths;
+    mats::explode(value, ',', &wavelengths);
+
+    if (hdr->band_size() > 0 && hdr->band_size() != wavelengths.size()) {
+      mainLog() << "Wavelengths, FWHM, band names and bad bands list all "
+                << "need to be the same length." << endl;
+      return false;
+    }
+
+    if (hdr->band_size() == 0) {
+      for (size_t i = 0; i < wavelengths.size(); i++) {
+        EnviBand* band = hdr->add_band();
+        band->set_center_wavelength(atof(trim(wavelengths[i]).c_str()));
+      }
+    } else {
+      for (size_t i = 0; i < wavelengths.size(); i++) {
+        hdr->mutable_band(i)->set_center_wavelength(
+            atof(trim(wavelengths[i]).c_str()));
+      }
+    }
+  } else if (tag_name == "fwhm") {
+    vector<string> fwhms;
+    mats::explode(value, ',', &fwhms);
+
+    if (hdr->band_size() > 0 && hdr->band_size() != fwhms.size()) {
+      mainLog() << "Wavelengths, FWHM, band names and bad bands list all "
+                << "need to be the same length." << endl;
+      return false;
+    }
+
+    if (hdr->band_size() == 0) {
+      for (size_t i = 0; i < fwhms.size(); i++) {
+        EnviBand* band = hdr->add_band();
+        band->set_fwhm(atof(trim(fwhms[i]).c_str()));
+      }
+    } else {
+      for (size_t i = 0; i < fwhms.size(); i++) {
+        hdr->mutable_band(i)->set_fwhm(atof(trim(fwhms[i]).c_str()));
+      }
+    }
+
+  } else if (tag_name == "bbl") {
+    vector<string> bbls;
+    mats::explode(value, ',', &bbls);
+
+    if (hdr->band_size() > 0 && hdr->band_size() != bbls.size()) {
+      mainLog() << "Wavelengths, FWHM, band names and bad bands list all "
+                << "need to be the same length." << endl;
+      return false;
+    }
+
+    if (hdr->band_size() == 0) {
+      for (size_t i = 0; i < bbls.size(); i++) {
+        EnviBand* band = hdr->add_band();
+        band->set_bad_band_multiplier(atof(trim(bbls[i]).c_str()));
+      }
+    } else {
+      for (size_t i = 0; i < bbls.size(); i++) {
+        hdr->mutable_band(i)->set_bad_band_multiplier(
+            atof(trim(bbls[i]).c_str()));
+      }
+    }
+  } else {
+    mainLog() << "Unrecognized tag name \"" << tag_name << "\"" << endl;
+    return false;
+  }
+
+  return true;
+}
+
+int EnviImageReader::GetOpenCVDType(int envi_dt, int& size) {
+  switch (envi_dt) {
+    case EnviImageHeader::BYTE8: size = 1; return CV_8UC1;
+    case EnviImageHeader::INT16: size = 2; return CV_16SC1;
+    case EnviImageHeader::INT32: size = 4; return CV_32SC1;
+    case EnviImageHeader::FLOAT32: size = 4; return CV_32FC1;
+    case EnviImageHeader::FLOAT64: size = 8; return CV_64FC1;
+    case EnviImageHeader::COMPLEX2x32:
+    case EnviImageHeader::COMPLEX2x64:
+      mainLog() << "Error: Complex inputs not yet supported." << endl;
+      break;
+    case EnviImageHeader::UINT16: size = 2; return CV_16UC1;
+    case EnviImageHeader::UINT32:
+    case EnviImageHeader::UINT64:
+    case EnviImageHeader::UINT64_LONG:
+      mainLog() << "Error: Unsupported data type in ENVI header." << endl;
+      break;
+    default:
+      mainLog() << "Error: Invalid data type in ENVI header." << endl;
+      break;
+  }
+
+  return -1;
+}
+
+}
---
+++ io/envi_image_reader.h
@@ -0,0 +1,131 @@
+// A file reader class for ENVI images.
+// Author: Philip Salvaggio
+
+#ifndef ENVI_IMAGE_READER_H
+#define ENVI_IMAGE_READER_H
+
+#include "base/endian.h"
+#include "base/macros.h"
+#include "io/envi_image_header.pb.h"
+
+#include <cstddef>
+#include <string>
+#include <vector>
+#include <opencv/cv.h>
+
+namespace mats_io {
+
+class EnviImageReader {
+ public:
+  // Default constructor.
+  EnviImageReader();
+
+  // Reads in an ENVI image, assumes the header is located in the same
+  // directory as the image, with the same filename, except for an additional
+  // ".hdr".
+  //
+  // Arguments:
+  //  image_filename  Full path to the image file.
+  //  hdr             Output variable containing the image metadata.
+  //  image           Output returned as a vector of cv::Mat objects, to avoid
+  //                  dealing with interleaving issues. The wavelengths can be
+  //                  found in the header object.
+  bool Read(const std::string& image_filename,
+            EnviImageHeader* hdr,
+            std::vector<cv::Mat>* image) {
+    return Read(image_filename, image_filename + ".hdr", hdr, image);
+  }
+
+  // Fully-specified Read method. Call this one if the header file does not
+  // follow the naming convention above
+  bool Read(const std::string& image_filename,
+            const std::string& header_filename,
+            EnviImageHeader* hdr,
+            std::vector<cv::Mat>* image);
+
+ private:
+  // Parse the header file and populate the EnviImageHeader object.
+  //
+  // Arguments:
+  //  header_filename  The filename of the ENVI header file
+  //  hdr              Output: EnviImageHeader to hold all of the metadata
+  bool ReadHeader(const std::string& header_filename,
+                  EnviImageHeader* hdr);
+
+  // Given a line from the header file, populate the appropriate header field.
+  // All multiline definitions are guaranteed to be collapsed into one line
+  // before this function is called, so all lines follow the format:
+  //   tag_name = value
+  //
+  // Arguments:
+  //  line  The line from the header file (may be a collapse of multiple lines)
+  //  hdr   Output: The EnviImageHeader to populate
+  bool ParseHeaderLine(const std::string& line, EnviImageHeader* hdr);
+
+  // Convert ENVI data types to OpenCV data types.
+  //
+  // Arguments:
+  //  envi_dt  The ENVI data type parsed from the header file.
+  //  size     Output: The number of bytes in the data type
+  //
+  // Returns:
+  //  The OpenCV data type, or -1 if the ENVI data type is not supported.
+  int GetOpenCVDType(int envi_dt, int& size);
+
+  // Calculate the 1-D index for a pixel given the row, column, band, and
+  // header information
+  template <class T>
+  void RearrangeImageData(T* data,
+                          EnviImageHeader* hdr,
+                          std::vector<cv::Mat>* image);
+
+ NO_COPY_OR_ASSIGN(EnviImageReader)
+};
+
+template <class T>
+void EnviImageReader::RearrangeImageData(T* data,
+                                        EnviImageHeader* hdr,
+                                        std::vector<cv::Mat>* image) {
+  const size_t kBands = hdr->bands();
+  const size_t kRows = hdr->lines();
+  const size_t kCols = hdr->samples();
+
+  bool needs_endian_swap = (hdr->byte_order() == 0 && !mats::isLittleEndian())
+                        || (hdr->byte_order() == 1 && mats::isLittleEndian());
+
+
+  size_t band_stride = 0, row_stride = 0, col_stride = 0;
+  switch (hdr->interleave_type()) {
+    case EnviImageHeader::BSQ:
+      band_stride = kRows * kCols;
+      row_stride = kCols;
+      col_stride = 1;
+      break;
+    case EnviImageHeader::BIL:
+      band_stride = kCols;
+      row_stride = kCols * kBands;
+      col_stride = 1;
+      break;
+    case EnviImageHeader::BIP:
+      band_stride = 1;
+      row_stride = kBands * kCols;
+      col_stride = kBands;
+      break;
+  }
+
+  for (size_t b = 0; b < kBands; b++) {
+    T* band_data = (T*)image->at(b).data;
+
+    for (size_t r = 0; r < kRows; r++) {
+      for (size_t c = 0; c < kCols; c++) {
+        T val = data[b * band_stride + r * row_stride + c * col_stride];
+        if (needs_endian_swap)  val = mats::swap_endian(val);
+        band_data[r * kCols + c] = val;
+      }
+    }
+  }
+}
+
+}
+
+#endif  // ENVI_IMAGE_READER_H
