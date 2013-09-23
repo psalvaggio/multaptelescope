a6717d024889029b88b9b5ccce278115d22fc47c
Philip Salvaggio <philipsalvaggio@reefshark.local>
2013-09-23 09:07:00 -0400

    Cleaned up the code.

--- deconvolution/constrained_least_squares.cpp
+++ deconvolution/constrained_least_squares.cpp
@@ -17,40 +17,39 @@ void ConstrainedLeastSquares::Deconvolve(const Mat& input,
   Mat input_fft;
   dft(input, input_fft, DFT_COMPLEX_OUTPUT);
 
-  vector<Mat> input_fft_planes;
-  split(input_fft, input_fft_planes);
+  Mat numerator;
+  mulSpectrums(input_fft, transfer_function, numerator, 0, true);
 
   Mat laplacian = Mat::zeros(input.rows, input.cols, CV_64FC1);
   double* laplacian_data = (double*) laplacian.data;
   laplacian_data[0*input.cols + 0] = 4;
-  laplacian_data[1*input.cols + 0] = -1;
   laplacian_data[0*input.cols + 1] = -1;
-  laplacian_data[0*input.rows + input.cols - 1] = -1;
+  laplacian_data[1*input.cols + 0] = -1;
+  laplacian_data[0*input.cols + input.cols - 1] = -1;
   laplacian_data[(input.rows - 1)*input.cols + 0] = -1;
 
   Mat laplacian_fft;
   dft(laplacian, laplacian_fft, DFT_COMPLEX_OUTPUT);
-
   vector<Mat> laplacian_fft_planes;
   split(laplacian_fft, laplacian_fft_planes);
+  Mat smooth_power = laplacian_fft_planes[0].mul(laplacian_fft_planes[0]) +
+                     laplacian_fft_planes[1].mul(laplacian_fft_planes[1]);
 
   vector<Mat> transfer_conj_planes;
   split(transfer_function, transfer_conj_planes);
-
   Mat transfer_power = transfer_conj_planes[0].mul(transfer_conj_planes[0]) +
                        transfer_conj_planes[1].mul(transfer_conj_planes[1]);
-  Mat smooth_power = laplacian_fft_planes[0].mul(laplacian_fft_planes[0]) +
-                     laplacian_fft_planes[1].mul(laplacian_fft_planes[1]);
-  Mat denominator = transfer_power + smoothness * smooth_power;
-  
-  divide(transfer_conj_planes[0], denominator, transfer_conj_planes[0]);
-  divide(transfer_conj_planes[1], denominator, transfer_conj_planes[1], -1);
+
+  Mat denominator = transfer_power + smooth_power * smoothness;
 
   vector<Mat> output_planes;
-  output_planes.push_back(transfer_conj_planes[0].mul(input_fft_planes[0]) -
-                          transfer_conj_planes[1].mul(input_fft_planes[1]));
-  output_planes.push_back(transfer_conj_planes[0].mul(input_fft_planes[1]) +
-                          transfer_conj_planes[1].mul(input_fft_planes[0]));
+  output_planes.push_back(Mat());
+  output_planes.push_back(Mat());
+
+  vector<Mat> numerator_planes;
+  split(numerator, numerator_planes);
+  divide(numerator_planes[0], denominator, output_planes[0]);
+  divide(numerator_planes[1], denominator, output_planes[1]);
 
   Mat output_fft;
   merge(output_planes, output_fft);
