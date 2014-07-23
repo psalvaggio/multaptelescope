280f313a21da4a94e34e4226f9998b6c5bea95af
Philip Salvaggio <pss7119@rit.edu>
2014-07-23 10:51:21 -0400

    Writes out images for an aberrated pupil function and its corresponding MTF.

---
+++ aberrated_otf.cpp
@@ -0,0 +1,61 @@
+// File Description
+// Author: Philip Salvaggio
+
+#include "base/detector_parameters.pb.h"
+#include "base/mats_init.h"
+#include "base/opencv_utils.h"
+#include "base/pupil_function.h"
+#include "base/simulation_config.pb.h"
+#include "base/telescope.h"
+#include "io/envi_image_header.pb.h"
+#include "io/logging.h"
+#include "optical_designs/aperture.h"
+
+#include <cstdlib>
+#include <iostream>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+#include <string>
+
+using namespace std;
+using namespace cv;
+using mats_io::Logging;
+
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    cerr << "Usage: ./mats_main base_dir" << endl;
+    return 1;
+  }
+
+  // Parse the base directory from the command line.
+  string base_dir(argv[1]);
+  if (base_dir[base_dir.size() - 1] != '/') {
+    base_dir.append("/");
+  }
+
+  mats::SimulationConfig sim_config;
+  mats::DetectorParameters detector_params;
+  vector<Mat> hyp_planes;
+  mats_io::EnviImageHeader hyp_header;
+  if (!mats::MatsInit(base_dir, &sim_config, &detector_params, NULL, NULL)) {
+    return 1;
+  }
+  sim_config.set_array_size(512);
+
+  for (size_t i = 0; i < 1; i++) {
+    mats::Telescope telescope(sim_config, i, detector_params);
+
+    mainLog() << "Focal Length: " << telescope.FocalLength() << " [m]" << endl;
+
+    Mat wfe = telescope.aperture()->GetWavefrontError();
+
+    mats::PupilFunction pupil;
+    telescope.aperture()->GetPupilFunction(wfe, 550e-9, &pupil);
+
+    cv::imwrite(base_dir + "wfe.png", ByteScale(pupil.real_part()));
+    cv::imwrite(base_dir + "mtf.png",
+        GammaScale(FFTShift(pupil.ModulationTransferFunction()), 1/2.2));
+  }
+
+  return 0;
+}
