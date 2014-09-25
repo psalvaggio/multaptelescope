bfcba04e360d8135a187a6d5be113579a90af558
Philip Salvaggio <pss7119@rit.edu>
2014-09-25 08:47:20 -0400

    Added a function to get a radial profile of the image.

--- base/opencv_utils.cpp
+++ base/opencv_utils.cpp
@@ -152,3 +152,44 @@ std::string GetMatDataType(const cv::Mat& mat) {
  
   return type.str();
 }
+
+void GetRadialProfile(const cv::Mat& input, double theta,
+                      std::vector<double>* output) {
+  if (!output) return;
+  output->clear();
+
+  const int rows = input.rows;
+  const int cols = input.cols;
+
+  int profile_size = std::min(rows, cols) / 2;
+  int center_x = cols / 2;
+  int center_y = rows / 2;
+
+  double dx = cos(theta);
+  double dy = sin(theta);
+
+  output->reserve(profile_size);
+  for (int i = 0; i < profile_size; i++) {
+    double x = center_x + i * dx;
+    double y = center_y + i * dy;
+
+    int x_lt = static_cast<int>(x);
+    int x_gt = x_lt + 1;
+    int y_lt = static_cast<int>(y);
+    int y_gt = y_lt + 1;
+
+    if (x_lt > 0 && y_lt > 0 && x_gt < cols && y_gt < rows) {
+      double alpha_x = x - x_lt;
+      double alpha_y = y - y_lt;
+      double inter_y_lt = (1-alpha_y) * input.at<double>(y_lt, x_lt) +
+                          alpha_y * input.at<double>(y_gt, x_lt);
+      double inter_y_gt = (1-alpha_y) * input.at<double>(y_lt, x_gt) +
+                          alpha_y * input.at<double>(y_gt, x_gt);
+      output->push_back((1-alpha_x) * inter_y_lt + alpha_x * inter_y_gt);
+    } else {
+      int x_rnd = std::max(std::min((int)round(x), cols - 1), 0);
+      int y_rnd = std::max(std::min((int)round(y), rows - 1), 0);
+      output->push_back(input.at<double>(y_rnd, x_rnd));
+    }
+  }
+}
--- base/opencv_utils.h
+++ base/opencv_utils.h
@@ -5,6 +5,7 @@
 #define OPENCV_UTILS_H
 
 #include <opencv/cv.h>
+#include <vector>
 
 cv::Mat ByteScale(const cv::Mat& input,
                   bool verbose = false);
@@ -61,5 +62,16 @@ void FFTShift(const cv::Mat& input, cv::Mat& output);
 cv::Mat FFTShift(const cv::Mat& input);
 
 std::string GetMatDataType(const cv::Mat& mat);
+
+// Gets a 1D profile of the 2D image. A profile  starts at the center and
+// extends to the extent of the inscribed circle of the frame. There will be
+// N/2 samples in the profile, where N is the smaller dimension of the image.
+//
+// Paramaters:
+//  input - The 2D image
+//  theta - Angle of the profile, CCW from +x axis
+//  output - Output: the profile.
+void GetRadialProfile(const cv::Mat& input, double theta,
+                      std::vector<double>* output);
 
 #endif  // OPENCV_UTILS_H
