9b7bda30730737710d971393a5ebfc6ccf46bc7b
Philip Salvaggio <pss7119@rit.edu>
2014-09-25 08:46:49 -0400

    A generic implementation of a genetic algorithm.

---
+++ genetic/genetic_algorithm.h
@@ -0,0 +1,116 @@
+// A generic interface for running genetic algorithms. To customize for your
+// specific needs, you will need to inherit from the GeneticAlgorithmImpl base
+// class. To be able to customize the data types used for models,
+// GeneticAlgorithmImpl is templated with the model class. The typedef model_t
+// will be available for use by subclasses. If the desired model type is a 
+// pointer, than you MUST use a smart pointer, or the memory will be leaked.
+// For efficiencies sake, the model type needs to be move-constructible and
+// move-assignable.
+//
+// Author: Philip Salvaggio
+
+#ifndef GENETIC_ALGORITHM_H
+#define GENETIC_ALGORITHM_H
+
+#include <vector>
+
+namespace genetic {
+
+// Run the genetic algorithm using the user-defined implementation.
+//
+// Parameters:
+//   impl                  The user-defined subclass of GeneticAlgorithmImpl
+//   population_size       The size of the population
+//   breeds_per_generation The number of new members to create every iteration
+//   best_model            Output: The best model found.
+template <typename Impl>
+void GeneticAlgorithm(Impl& impl,
+                      size_t population_size,
+                      size_t breeds_per_generation,
+                      typename Impl::model_t& best_model);
+
+// Utility class for wrapping a model and its fitness.
+template <typename Model>
+class PopulationMember {
+ public:
+  PopulationMember(Model&& model, double fitness);
+  explicit PopulationMember(Model&& model);
+  PopulationMember(PopulationMember<Model>&& other);
+
+  PopulationMember& operator=(PopulationMember<Model>&& other);
+
+  Model& model() { return model_; }
+  const Model& model() const { return model_; }
+  
+  double fitness() const { return fitness_; }
+  void set_fitness(double fitness) { fitness_ = fitness; }
+
+ private:
+  Model model_;
+  double fitness_;
+};
+
+template <typename Model>
+class GeneticAlgorithmImpl {
+ public:
+  // Typedef available for user-defined classes to use.
+  using model_t = Model;
+
+  // Evaluate should evaluate the user-defined fitness function. The result
+  // should be stored using set_fitness.
+  //
+  // Parameters:
+  //   member  The model to evalute.
+  //
+  // Returns:
+  //   A boolean indicating whether the model is valid. If false, the fitness
+  //   stroed using set_fitness is ignored and the model is dropped from the
+  //   population.
+  virtual bool Evaluate(PopulationMember<Model>& member) = 0;
+
+  // Introduce a new model into the population. It is assumed that the model is
+  // valid, i.e. Evalute() would return true.
+  virtual model_t Introduce() = 0;
+
+  // Perform the genetic crossover opertation. Given two parent models from the
+  // population, constructs a new model. The new model is not necessary valid.
+  // Any non-determinism should be introduced by the subclass, as this function
+  // is called every generation to create the new population members.
+  //
+  // Parameters:
+  //   member1  The first model
+  //   member2  The second model
+  //
+  // Returns:
+  //   The model that resulted from the crossover operation.
+  virtual model_t Crossover(const PopulationMember<Model>& member1,
+                            const PopulationMember<Model>& member2) = 0;
+
+  // Performs the mutation operation on the given model. This is called on
+  // every new population member produced by Crossover, so any non-determinism
+  // must be introduced by the subclass. The mutation may result in the model
+  // becoming invalid.
+  //
+  // Parameters:
+  //   member  Input/Output: The model to be modified in-place.
+  virtual void Mutate(PopulationMember<Model>& member) = 0;
+
+  // A convergence test.
+  //
+  // Parameters:
+  //   population      The collection of models currently in the population. 
+  //                   Their fitnesses will have been computed with Evaluate.
+  //   generation_num  The generation number we are on.
+  //
+  // Returns:
+  //   Whether the iteration should continue.
+  virtual bool ShouldContinue(
+      const std::vector<PopulationMember<Model>>& population,
+      size_t generation_num) = 0;
+};
+
+}  // namespace genetic
+
+#include "genetic_algorithm.hpp"
+
+#endif  // GENETIC_ALGORITHM_H
---
+++ genetic/genetic_algorithm.hpp
@@ -0,0 +1,194 @@
+// Implementation file for genetic_algorithm.h
+// Author: Philip Salvaggio
+
+#ifndef GENETIC_ALGORITHM_HPP
+#define GENETIC_ALGORITHM_HPP
+
+#include <algorithm>
+#include <iostream>
+#include "genetic_algorithm.h"
+
+namespace genetic {
+
+template <typename Impl>
+void GeneticAlgorithm(Impl& impl,
+                      size_t population_size,
+                      size_t breeds_per_generation,
+                      typename Impl::model_t& best_model) {
+  using Model = typename Impl::model_t;
+
+  // A lambda for sorting the population based on fitness via std::sort.
+  auto fitness_sort =
+      [](const PopulationMember<Model>& a,
+         const PopulationMember<Model>& b) -> bool {
+          return a.fitness() > b.fitness();
+      };
+
+  // Randomly selects an index based off of a given probability distribution.
+  // The CDF does not need to be normalized, just monotonically increasing.
+  auto select_index =
+      [](const std::vector<double>& cdf) -> int {
+        double p = (double)rand() / RAND_MAX * cdf.back();
+        int i = 0;
+        while (p > cdf[i]) i++;
+        return i;
+      };
+
+  // Given the population, construct a cumulative distribution function based
+  // on the fitness of the members. Despite the name, the CDF is not
+  // normalized.
+  auto compute_fitness_cdf =
+      [&fitness_sort](const std::vector<PopulationMember<Model>>& population,
+                      std::vector<double>& fitness_cdf) -> void {
+        fitness_cdf.resize(population.size());
+        auto min_member = std::max_element(
+            std::begin(population), std::end(population), fitness_sort);
+        double min_fitness = min_member->fitness();
+        
+        int index = 0;
+        for (const PopulationMember<Model>& tmp : population) {
+          fitness_cdf[index] = (index == 0) ?  tmp.fitness() - min_fitness
+              : fitness_cdf[index-1] + tmp.fitness() - min_fitness;
+          index++;
+        }
+      };
+
+  // Initialize the population.
+  std::vector<PopulationMember<Model>> population;
+  population.reserve(population_size + breeds_per_generation);
+  for (size_t i = 0; i < population_size; i++) {
+    PopulationMember<Model> member(std::move(impl.Introduce()), 0);
+    impl.Evaluate(member);
+    population.push_back(std::move(member));
+  }
+  std::sort(std::begin(population), std::end(population), fitness_sort);
+
+  std::vector<double> cumulative_fitness(population_size, 0);
+  std::vector<double> new_cumulative_fitness(
+      population_size + breeds_per_generation, 0);
+  std::vector<size_t> selection_indices(population_size, 0);
+
+  std::cout << std::endl << "Generation 0, Fitness = "
+            << population[0].fitness();
+  size_t generation_num = 0;
+
+  // Main loop of the genetic algorithm.
+  do {
+    generation_num++;
+
+    compute_fitness_cdf(population, cumulative_fitness);
+    /*
+      std::cout << std::endl;
+      std::cout << "Fitness: [";
+      for (auto& tmp : population) {
+        std::cout << tmp.fitness() << " ";
+      }
+      std::cout << "]" << std::endl;
+      std::cout << "CDF: [";
+      for (auto& tmp : cumulative_fitness) {
+        std::cout << tmp << " ";
+      }
+      std::cout << "]" << std::endl;
+      */
+
+    // Construct the new population members from the existing ones.
+    for (size_t i = 0; i < breeds_per_generation; i++) {
+      int index1 = select_index(cumulative_fitness);
+      int index2 = index1;
+      while (index1 == index2) {
+        index2 = select_index(cumulative_fitness);
+      }
+
+      do {
+        PopulationMember<Model> member(
+            std::move(impl.Crossover(population[index1], population[index2])));
+        impl.Mutate(member);
+
+        // Only add if the model is valid.
+        if (impl.Evaluate(member)) {
+          population.push_back(std::move(member));
+          break;
+        }
+      } while (true);
+    }
+
+    // If we extra members hanging around from the previous generation, select
+    // the best population member and fill out the population using the fitness
+    // to weight selection.
+    if (population.size() > population_size) {
+      compute_fitness_cdf(population, new_cumulative_fitness);
+      std::cout << std::endl << "Bred CDF: [";
+      for (auto& tmp : new_cumulative_fitness) {
+        std::cout << tmp << " ";
+      }
+      std::cout << "]" << std::endl;
+
+      selection_indices[0] = 0;
+      for (size_t i = 1; i < population_size; i++) {
+        bool unique = false;
+        size_t index = 0;
+        int iter = 0;
+
+        while (!unique) {
+          iter++;
+          index = select_index(new_cumulative_fitness);
+          unique = true;
+          for (int j = i - 1; j >= 0; j--) {
+            if (selection_indices[j] == index) {
+              unique = false;
+              break;
+            }
+          }
+        }
+        selection_indices[i] = index;
+      }
+
+      // Sort the selection indices in ascending order, so we can select them in
+      // the population in one pass.
+      std::sort(std::begin(selection_indices), std::end(selection_indices));
+
+      // Construct the new population based on the selections.
+      for (size_t i = 0; i < population_size; i++) {
+        if (i == selection_indices[i]) continue;
+        std::swap(population[i], population[selection_indices[i]]);
+      }
+      population.erase(std::begin(population) + population_size,
+                       std::end(population));
+    }
+
+    std::sort(std::begin(population), std::end(population), fitness_sort);
+
+    std::cout << "\rGeneration " << generation_num
+              << ", Fitness = " << population[0].fitness();
+    std::cout.flush();
+    impl.Visualize(population[0].model());
+  } while (impl.ShouldContinue(population, generation_num));
+
+  std::cout << std::endl;
+
+  best_model = Model(population[0].model());
+}
+
+template <typename Model>
+PopulationMember<Model>::PopulationMember(Model&& model, double fitness)
+    : model_(std::move(model)), fitness_(fitness) {}
+
+template <typename Model>
+PopulationMember<Model>::PopulationMember(Model&& model)
+    : PopulationMember(std::move(model), 0) {}
+
+template <typename Model>
+PopulationMember<Model>::PopulationMember(PopulationMember<Model>&& other)
+    : PopulationMember(std::move(other.model_), other.fitness_) {}
+
+template <typename Model>
+PopulationMember<Model>& PopulationMember<Model>::operator=(
+    PopulationMember<Model>&& other) {
+  model_ = std::move(other.model_);
+  fitness_ = other.fitness_;
+  return *this;
+}
+
+}  // namespace genetic
+
+#endif  // GENETIC_ALGORITHM_HPP
