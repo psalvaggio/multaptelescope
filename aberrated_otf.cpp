d9f5f0d36dc40ec0e63752d6d91067442cec0cf7
Philip Salvaggio <pss7119@rit.edu>
2015-03-12 09:48:42 -0400

    You can now specify the simulation id on the command line.

--- aberrated_otf.cpp
+++ aberrated_otf.cpp
@@ -26,7 +26,7 @@ using mats_io::Logging;
 
 int main(int argc, char** argv) {
   if (argc < 2) {
-    cerr << "Usage: ./mats_main base_dir" << endl;
+    cerr << "Usage: ./mats_main base_dir [sim_id]" << endl;
     return 1;
   }
 
@@ -45,20 +45,30 @@ int main(int argc, char** argv) {
   }
   sim_config.set_array_size(512);
 
-  for (size_t i = 0; i < 1; i++) {
-    mats::Telescope telescope(sim_config, i, detector_params);
-
-    Mat wfe = telescope.aperture()->GetWavefrontError();
-
-    mats::PupilFunction pupil;
-    telescope.aperture()->GetPupilFunction(wfe,
-        sim_config.reference_wavelength(), &pupil);
-
-    Mat mtf = pupil.ModulationTransferFunction();
-
-    cv::imwrite(base_dir + "wfe.png", ByteScale(pupil.phase(), true));
-    cv::imwrite(base_dir + "mtf.png", GammaScale(FFTShift(mtf), 1/2.2));
+  int sim_index = 0;
+  if (argc >= 3) {
+    int sim_id = atoi(argv[2]);
+    for (int i = 0; i < sim_config.simulation_size(); i++) {
+      if (sim_config.simulation(i).simulation_id() == sim_id) {
+        sim_index = i;
+        break;
+      }
+    }
   }
+
+  mats::Telescope telescope(sim_config, sim_index, detector_params);
+
+  Mat wfe = telescope.aperture()->GetWavefrontError();
+
+  mats::PupilFunction pupil;
+  telescope.aperture()->GetPupilFunction(wfe,
+      sim_config.reference_wavelength(), &pupil);
+
+  Mat mtf = pupil.ModulationTransferFunction();
+
+  cv::imwrite(base_dir + "mask.png", ByteScale(pupil.magnitude()));
+  cv::imwrite(base_dir + "wfe.png", ByteScale(pupil.phase()));
+  cv::imwrite(base_dir + "mtf.png", GammaScale(FFTShift(mtf), 1/2.2));
 
   return 0;
 }
