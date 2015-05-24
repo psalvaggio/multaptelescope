6ebb8326e9898e86379c9cecd6c78ddd06d6a370
Philip Salvaggio <pss7119@rit.edu>
2015-05-24 08:12:07 -0400

    Added a scandir() method that lets you pass in the the extension you want.

--- base/filesystem.cpp
+++ base/filesystem.cpp
@@ -2,7 +2,11 @@
 // Author: Philip Salvaggio
 
 #include "filesystem.h"
+
+#include "base/str_utils.h"
+
 #include <sys/stat.h>
+#include <dirent.h>
 
 namespace mats {
 
@@ -15,6 +19,27 @@ bool is_dir(const std::string& path) {
 bool file_exists(const std::string& path) {
   struct stat buffer;   
   return stat(path.c_str(), &buffer) == 0;
+}
+
+void scandir(const std::string& path,
+             const std::string& extension,
+             std::vector<std::string>* files) {
+  files->clear();
+
+  DIR* dir = opendir(path.c_str());
+  if (!dir) return;
+
+  struct dirent entry;
+  struct dirent* result;
+
+  while (readdir_r(dir, &entry, &result) == 0) {
+    if (result == NULL) break;
+    if (mats::ends_with(entry.d_name, extension)) {
+      files->emplace_back(entry.d_name);
+    }
+  }
+
+  closedir(dir);
 }
 
 }
--- base/filesystem.h
+++ base/filesystem.h
@@ -5,11 +5,15 @@
 #define FILESYSTEM_H
 
 #include <string>
+#include <vector>
 
 namespace mats {
 
 bool is_dir(const std::string& path);
 bool file_exists(const std::string& path);
+void scandir(const std::string& path,
+             const std::string& extension,
+             std::vector<std::string>* files);
 
 }
 
