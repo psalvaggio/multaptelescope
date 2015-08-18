6714480c66135090dfc4e7bf4d0087f32d8facfa
Philip Salvaggio <pss7119@rit.edu>
2015-08-18 10:16:47 -0400

    Now uses a global frame index and does zero-padding on the frame numbers.

--- acquire_images.cpp
+++ acquire_images.cpp
@@ -14,6 +14,8 @@ using mats_io::SbigDetector;
 DEFINE_bool(cooled, false, "Whether to cool the detector.");
 DEFINE_int32(exposure_time, 1000, "Exposure time [hundreths of a second]");
 
+static int frame_index = 0;
+
 void die(short, short) {
   cout << "Shutting down due to error." << endl;
   exit(1);
@@ -28,16 +30,32 @@ void AcquireImages(SbigDetector& detector,
   detector.GetSize(fr_width, fr_height);
   vector<uint16_t> roi{0, 0, fr_width, fr_height};
 
+  auto Save = [&output_dir] (Mat& image, int frame_num) {
+    string filename =
+        mats::StringPrintf("%sframe%03d.png", output_dir.c_str(), frame_num);
+    imwrite(filename, image);
+    cout << "Acquired frame " << frame_num << endl;
+  };
 
   Mat full_frame;
-  for (int i = 0; i < num_images; i++) {
-    detector.Capture(0, 0, fr_width, fr_height, FLAGS_exposure_time,
-                     &full_frame);
+  if (num_images == 0) {
+    int key;
+    do {
+      key = cv::waitKey(0);
+      if (key == 13) {
+        detector.Capture(0, 0, fr_width, fr_height, FLAGS_exposure_time,
+                         &full_frame);
 
-    string filename =
-        mats::StringPrintf("%sframe%d.png", output_dir.c_str(), i);
-    imwrite(filename, full_frame);
-    cout << "Acquired frame " << i << endl;
+        Save(full_frame, frame_index++);
+      }
+    } while (key != 'q');
+  } else {
+    for (int i = 0; i < num_images; i++) {
+      detector.Capture(0, 0, fr_width, fr_height, FLAGS_exposure_time,
+                       &full_frame);
+
+      Save(full_frame, frame_index++);
+    }
   }
 }
 
@@ -53,10 +71,15 @@ int main(int argc, char** argv) {
     detector.Cool(-15);
   }
 
+  cout << "Acquisition Type:" << endl
+       << " n > 0: Acquire n images" << endl
+       << " n = 0: On-demand (Enter to capture, q to quit)" << endl
+       << " n < 0: Quit" << endl;
+
   int num_frames;
   string output_dir = ".";
   for (;;) {
-    cout << "Number of images (negative to quit): ";
+    cout << "Acquisition Type: ";
     cin >> num_frames;
     if (num_frames < 0) break;
 
