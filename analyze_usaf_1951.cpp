35a88609cc7c15fecfbfe2c8cd85593533ca5abc
Philip Salvaggio <pss7119@rit.edu>
2015-09-22 10:51:19 -0400

    Added the main program for USAF-1951 target analysis.

---
+++ analyze_usaf_1951.cpp
@@ -0,0 +1,83 @@
+// Analyze a USAF-1951 tri-bar target by taking profiles over the various
+// tri-bar groups.
+// Author: Philip Salvaggio
+
+#include "mats.h"
+
+#include <gflags/gflags.h>
+
+using namespace std;
+using namespace cv;
+using namespace mats;
+
+DEFINE_string(image, "", "Image filename");
+DEFINE_int32(levels, 2, "Number of nested USAF targets.");
+DEFINE_string(output_dir, ".", "Directory into which to store the output.");
+
+int main(int argc, char** argv) {
+  google::ParseCommandLineFlags(&argc, &argv, true);
+
+  // Get the Tri-bar target region in the image
+  Mat image;
+  {
+    Mat image_full = imread(ResolvePath(FLAGS_image), 0);
+    if (!image_full.data) {
+      cerr << "Could not read image file." << endl;
+      return 1;
+    }
+
+    vector<uint16_t> roi = GetRoi(image_full);
+    image_full(Range(roi[1], roi[1] + roi[3]),
+               Range(roi[0], roi[0] + roi[2])).copyTo(image);
+  }
+
+  // Validate the output directory
+  string dir = ResolvePath(FLAGS_output_dir);
+  if (!is_dir(dir)) {
+    cerr << "Output directory " << dir << " does not exist or is not a "
+         << "directory." << endl;
+    return 1;
+  }
+
+  // Perform recognition
+  Usaf1951Target tribar(image, FLAGS_levels);
+  if (!tribar.RecognizeTarget()) return 1;
+
+  // Output the profiles
+  for (int i = 0; i < tribar.num_bar_groups(); i++) {
+    if (!tribar.FoundBarGroup(i)) continue;
+
+    vector<pair<double, double>> profile;
+    string filename;
+
+    tribar.GetProfile(i, Usaf1951Target::HORIZONTAL, &profile);
+    filename = StringPrintf("%sgroup_%02d_horizontal.txt", dir.c_str(), i);
+
+    ofstream horiz_ofs(filename);
+    if (!horiz_ofs.is_open()) continue;
+
+    for (size_t j = 0; j < profile.size(); j++) {
+      horiz_ofs << profile[j].first << "\t" << profile[j].second << endl;
+    }
+
+    tribar.GetProfile(i, Usaf1951Target::VERTICAL, &profile);
+    filename = StringPrintf("%s/group_%02d_vertical.txt", dir.c_str(), i);
+
+    ofstream vert_ofs(filename);
+    if (!vert_ofs.is_open()) continue;
+
+    for (size_t j = 0; j < profile.size(); j++) {
+      vert_ofs << profile[j].first << "\t" << profile[j].second << endl;
+    }
+  }
+
+  // Make two diagnostic outputs so the user can see if anything messed up
+  Mat output = tribar.VisualizeBoundingBoxes();
+  imwrite(StringPrintf("%sbounding_boxes.png", dir.c_str()), output);
+
+  output = tribar.VisualizeProfileRegions();
+  imwrite(StringPrintf("%sprofile_regions.png", dir.c_str()),
+          ByteScale(output));
+
+  return 0;
+}
