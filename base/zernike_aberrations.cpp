048501024b05c073a86405cb2138cf6303455079
Philip Salvaggio <pss7119@rit.edu>
2015-10-16 10:03:11 -0400

    Removed unused aberrations.

--- base/zernike_aberrations.cpp
+++ base/zernike_aberrations.cpp
@@ -62,62 +62,6 @@ void ZernikeAberrations::aberrations(const vector<double>& weights,
             wfe += weights[7] * rho * (3 * rho * rho - 2) * sin_theta; break;
           case 8:
             wfe += weights[8] * (1 - 6 * rho * rho + 6 * pow(rho, 4)); break;
-          case 9:
-            wfe += weights[9] * rho * rho * rho * cos(3 * theta); break;
-          case 10:
-            wfe += weights[10] * rho * rho * rho * sin(3 * theta); break;
-          case 11:
-            wfe += weights[11] * rho * rho * (4 * rho * rho - 3) *
-                   cos(2 * theta);
-            break;
-          case 12:
-            wfe += weights[12] * rho * rho * (4 * rho * rho - 3) *
-                   sin(2 * theta);
-            break;
-          case 13:
-            wfe += weights[13] * rho * (3 - 12 * rho*rho + 10 * pow(rho, 4)) *
-                   cos_theta;
-            break;
-          case 14:
-            wfe += weights[14] * rho * (3 - 12 * rho*rho + 10 * pow(rho, 4)) *
-                   sin_theta;
-            break;
-          case 15:
-            wfe += weights[15] * (-1 + 12 * rho*rho - 30 * pow(rho, 4) + 20 *
-                   pow(rho, 6));
-            break;
-          case 16:
-            wfe += weights[16] * pow(rho, 4) * cos(4 * theta); break;
-          case 17:
-            wfe += weights[17] * pow(rho, 4) * sin(4 * theta); break;
-          case 18:
-            wfe += weights[18] * pow(rho, 3) * (5 * rho*rho - 4) *
-                   cos(3 * theta);
-            break;
-          case 19:
-            wfe += weights[19] * pow(rho, 3) * (5 * rho*rho - 4) *
-                   sin(3 * theta);
-            break;
-          case 20:
-            wfe += weights[20] * rho * rho * (6 - 20 * rho*rho + 15 *
-                   pow(rho, 4)) * cos(2 * theta);
-            break;
-          case 21:
-            wfe += weights[21] * rho * rho * (6 - 20 * rho*rho + 15 *
-                   pow(rho, 4)) * sin(2 * theta);
-            break;
-          case 22:
-            wfe += weights[22] * rho * (-4 + 30 * rho*rho - 60 * pow(rho, 4) +
-                   35 * pow(rho, 6)) * cos(theta);
-            break;
-          case 23:
-            wfe += weights[23] * rho * (-4 + 30 * rho*rho - 60 * pow(rho, 4) +
-                   35 * pow(rho, 6)) * sin(theta);
-            break;
-          case 24:
-            wfe += weights[24] * (1 - 20 * rho*rho + 90 * pow(rho, 4) - 140 *
-                   pow(rho, 6) + 70 * pow(rho, 8));
-            break;
         }
       }
       output->at<double>(i, j) = wfe;
