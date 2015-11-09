9c00e03156f09606b66f00ee8444d798b7d8bc03
Philip Salvaggio <philip.salvaggio@me.com>
2015-11-09 08:37:41 -0500

    Updated to reflect API changes

--- aperture_optimization/golay_fitness_function.cpp
+++ aperture_optimization/golay_fitness_function.cpp
@@ -7,10 +7,11 @@
 #include "base/opencv_utils.h"
 #include "optical_designs/compound_aperture.h"
 
-#include <opencv/cv.h>
-#include <opencv/highgui.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 using namespace std;
+using namespace mats;
 
 namespace genetic {
 
@@ -22,21 +23,22 @@ GolayFitnessFunction::GolayFitnessFunction(int num_subapertures,
       max_center_radius2_(0),
       subaperture_diameter2_(subaperture_diameter*subaperture_diameter) {
 
+  // Set up the simulation parameters, so we can just change the subaperture 
+  // locations later.
   max_center_radius2_ = pow(0.5*(encircled_diameter-subaperture_diameter), 2);
   conf_.set_array_size(512);
   conf_.set_reference_wavelength(reference_wavelength);
 
-  mats::Simulation* sim = conf_.add_simulation();
-  mats::ApertureParameters* compound_params = sim->mutable_aperture_params();
+  Simulation* sim = conf_.add_simulation();
+  auto* compound_params = sim->mutable_aperture_params();
   compound_params->set_encircled_diameter(encircled_diameter);
 
-  compound_params->set_type(mats::ApertureParameters::COMPOUND);
-  CompoundApertureParameters* array_ext =
-      compound_params->MutableExtension(compound_aperture_params);
+  compound_params->set_type(ApertureParameters::COMPOUND);
+  auto* array_ext = compound_params->MutableExtension(compound_aperture_params);
   array_ext->set_combine_operation(CompoundApertureParameters::OR);
   for (int i = 0; i < num_subapertures; i++) {
-    mats::ApertureParameters* subap = array_ext->add_aperture();
-    subap->set_type(mats::ApertureParameters::CIRCULAR);
+    auto* subap = array_ext->add_aperture();
+    subap->set_type(ApertureParameters::CIRCULAR);
     subap->set_encircled_diameter(subaperture_diameter);
     subap->set_offset_x(0);
     subap->set_offset_y(0);
@@ -57,63 +59,65 @@ bool GolayFitnessFunction::operator()(PopulationMember<model_t>& member) {
       double dx = locations[i] - locations[j];
       double dy = locations[i+1] - locations[j+1];
       double distance2 = dx*dx + dy*dy;
-      if (distance2 < subaperture_diameter2_)  return false;
+      if (distance2 < subaperture_diameter2_) return false;
     }
   }
 
-  mats::ApertureParameters* ap_params(
-      conf_.mutable_simulation(0)->mutable_aperture_params());
-  CompoundApertureParameters* compound_params =
-      ap_params->MutableExtension(compound_aperture_params);
+  // Set the subaperture positions in our parameters
+  auto* ap_params = conf_.mutable_simulation(0)->mutable_aperture_params();
+  auto* compound_params = ap_params->MutableExtension(compound_aperture_params);
   for (size_t i = 0; i < locations.size(); i += 2) {
-    mats::ApertureParameters* subap = compound_params->mutable_aperture(i/2);
+    auto* subap = compound_params->mutable_aperture(i/2);
     subap->set_offset_x(locations[i]);
     subap->set_offset_y(locations[i+1]);
   }
 
-  mats::PupilFunction pupil;
-  unique_ptr<Aperture> aperture(ApertureFactory::Create(conf_, 0));
-  aperture->GetPupilFunction(550e-9, &pupil);
-  cv::Mat mtf = FFTShift(pupil.ModulationTransferFunction());
-  
-  cv::imshow("MTF", GammaScale(mtf, 1/2.2));
-  cv::imshow("Mask", ByteScale(aperture->GetApertureMask()));
-  cv::waitKey(1);
+  // Create the proposed aperture
+  unique_ptr<Aperture> aperture(ApertureFactory::Create(conf_.simulation(0)));
 
-  model_t peaks;
-  GetAutocorrelationPeaks(member.model(), &peaks);
-  for (size_t i = 0; i < peaks.size(); i += 2) {
-    moment_of_inertia += peaks[i]*peaks[i] + peaks[i+1]*peaks[i+1];
-  }
-  double compactness = 1 / moment_of_inertia;
+  // Calculate the monochromatic, on-axis MTF
+  PupilFunction pupil(conf_.array_size(), conf_.reference_wavelength());
+  aperture->GetPupilFunction(conf_.reference_wavelength(), 0, 0, &pupil);
+  cv::Mat mtf = pupil.ModulationTransferFunction();
+
+  // Calculate the non-redencance of the MTF (as a percentage)
   cv::Mat mtf_float;
   mtf.convertTo(mtf_float, CV_32F);
   cv::threshold(mtf_float, mtf_float, 0.03, 1, cv::THRESH_TOZERO);
   int non_zeros = cv::countNonZero(mtf_float);
   double support_frac = non_zeros / (M_PI * pow(mtf.cols / 2, 2));
-  double fitness = support_frac + 0.05 * compactness;
+  
+  // Calculate the compactness of the MTF
+  model_t peaks;
+  GetAutocorrelationPeaks(member.model(), &peaks);
+  for (size_t i = 0; i < peaks.size(); i += 2) {
+    moment_of_inertia += (peaks[i]*peaks[i] + peaks[i+1]*peaks[i+1]) / 
+                         max_center_radius2_;
+  }
+  double compactness = 1 / moment_of_inertia;
 
+  double fitness = support_frac + 1 * compactness;
   member.set_fitness(fitness);
 
   return true;
 }
 
 void GolayFitnessFunction::Visualize(const model_t& locations) {
-  mats::ApertureParameters* ap_params(
+  ApertureParameters* ap_params(
       conf_.mutable_simulation(0)->mutable_aperture_params());
   CompoundApertureParameters* compound_params =
       ap_params->MutableExtension(compound_aperture_params);
   for (size_t i = 0; i < locations.size(); i += 2) {
-    mats::ApertureParameters* subap = compound_params->mutable_aperture(i/2);
+    ApertureParameters* subap = compound_params->mutable_aperture(i/2);
     subap->set_offset_x(locations[i]);
     subap->set_offset_y(locations[i+1]);
   }
 
-  mats::PupilFunction pupil;
-  unique_ptr<Aperture> aperture(new CompoundAperture(conf_, 0));
-  aperture->GetPupilFunction(550e-9, &pupil);
+  PupilFunction pupil(conf_.array_size(), conf_.reference_wavelength());
+  unique_ptr<Aperture> aperture(ApertureFactory::Create(conf_.simulation(0)));
+  aperture->GetPupilFunction(550e-9, 0, 0, &pupil);
   cv::Mat mtf = pupil.ModulationTransferFunction();
-  cv::Mat mask = ByteScale(aperture->GetApertureMask());
+  cv::Mat mask = ByteScale(aperture->GetApertureMask(512));
   cv::circle(mask, cv::Point(mask.cols / 2, mask.rows / 2), mask.cols / 2,
              cv::Scalar(255, 255, 0), 2);
   cv::imshow("Best MTF", FFTShift(GammaScale(mtf, 1/2.2)));
@@ -121,47 +125,13 @@ void GolayFitnessFunction::Visualize(const model_t& locations) {
   cv::waitKey(1);
 }
 
-//  double enc_diameter = aperture->encircled_diameter();
-
-//  double fitness = 0;
-//  std::vector<double> angles{0, 30, 60, 90, 120, 150};
-//  for (double angle : angles) {
-//    std::vector<double> profile;
-//    GetRadialProfile(mtf, angle * M_PI / 180, &profile);
-//
-//    int last_significant_index = 0;
-//    for (int i = profile.size() - 1; i >= 0; i--) {
-//      if (profile[i] > 1e-3) {
-//        last_significant_index = i;
-//        break;
-//      }
-//    }
-//    double diameter = enc_diameter *
-//                      (double)last_significant_index / profile.size();
-//
-//    double smoothness = 0;
-//    for (size_t i = 1; i <= last_significant_index; i++) {
-//      smoothness += fabs(profile[i-1] - profile[i]);
-//    }
-//    smoothness = 1 - smoothness;
-//
-//    fitness += smoothness + 0.5 * diameter;
-//    //fitness += diameter;
-//  }
-  
-//  cv::Scalar sum = cv::sum(mtf);
-//  double diameter = aperture->encircled_diameter();
-//  double fitness = diameter + 1e-4 * sum[0] / (M_PI * diameter * diameter / 4);
-//  member.set_fitness(fitness);
-//  return true;
-
 
 void GolayFitnessFunction::GetAutocorrelationPeaks(const model_t& locations,
                                                    model_t* peaks) {
   if (!peaks) return;
-  peaks->resize(locations.size() * (locations.size() - 1));
+  peaks->resize(locations.size() * (locations.size() - 1) + 1, 0);
 
-  int index = 0;
+  int index = 1;
   for (size_t i = 0; i < locations.size(); i += 2) {
     for (size_t j = 0; j < locations.size(); j += 2) {
       if (i == j) continue;
--- aperture_optimization/golay_fitness_function.h
+++ aperture_optimization/golay_fitness_function.h
@@ -1,4 +1,4 @@
-// File Description
+// A genetic algorithm fitness function for generating Golay apertures.
 // Author: Philip Salvaggio
 
 #ifndef GOLAY_GENETIC_IMPL_H
@@ -31,86 +31,6 @@ class GolayFitnessFunction
   double max_center_radius2_;
   double subaperture_diameter2_;
 };
-
-/*
-class GolayGeneticImpl : public GeneticAlgorithmImpl<std::vector<double>> {
- public:
-  GolayGeneticImpl(int num_subapertures,
-                   double max_center_radius,
-                   double subaperture_diameter,
-                   double mutate_probability,
-                   double crossover_probability);
-
-  bool Evaluate(PopulationMember<model_t>& member) override;
-
-  model_t Introduce() override;
-
-  model_t Crossover(const PopulationMember<model_t>& member1,
-                    const PopulationMember<model_t>& member2) override;
-
-  void Mutate(PopulationMember<model_t>& member) override;
-
-  bool ShouldContinue(
-      const std::vector<PopulationMember<model_t>>&, size_t) override {
-    return should_continue_;
-  }
-
-  void Stop() { should_continue_ = false; }
-
-  void Visualize(const model_t& model) override;
-
-  void ZeroMean(model_t* locations);
-
-  void GetAutocorrelationPeaks(const model_t& locations, model_t* peaks);
-
- private:
-  int num_subapertures_;
-  double max_center_radius2_;
-  double subaperture_diameter2_;
-  bool should_continue_;
-  double mutate_probability_;
-  double crossover_probability_;
-
-  mats::SimulationConfig conf_;
-};
-
-class GolayLocalImpl : public GeneticAlgorithmImpl<std::vector<double>> {
-  public:
-   GolayLocalImpl(const model_t& best_guess,
-                  double encircled_diameter,
-                  double subaperture_diameter,
-                  double mutate_probability);
-
-  bool Evaluate(PopulationMember<model_t>& member) override;
-
-  model_t Introduce() override;
-
-  model_t Crossover(const PopulationMember<model_t>& member1,
-                    const PopulationMember<model_t>& member2) override;
-
-  void Mutate(PopulationMember<model_t>& member) override;
-
-  bool ShouldContinue(
-      const std::vector<PopulationMember<model_t>>&, size_t) override {
-    return should_continue_;
-  }
-
-  void Stop() { should_continue_ = false; }
-
-  void Visualize(const model_t& model) override;
-
-  void GetAutocorrelationPeaks(const model_t& locations, model_t* peaks);
-
- private:
-  int num_subapertures_;
-  double max_center_radius2_;
-  double subaperture_diameter2_;
-  bool should_continue_;
-  LocalSparseAperture<model_t> local_searcher_;
-
-  mats::SimulationConfig conf_;
-};
-*/
 
 }  // namespace genetic
 
