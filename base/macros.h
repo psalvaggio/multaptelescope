3fde2f4656ca34ccdda4a3bf423321ca420bb9b4
Philip Salvaggio <philip.salvaggio@me.com>
2015-11-09 13:28:36 -0500

    Removed unused macros, deleted copy constructor/assignment instead of declaring them private.

--- base/macros.h
+++ base/macros.h
@@ -4,26 +4,16 @@
 #ifndef MACROS_H
 #define MACROS_H
 
-#define NO_COPY_OR_ASSIGN(class_name) \
- private: \
-  class_name(const class_name& other); \
-  class_name& operator=(const class_name& other);
-
-#define NO_CONSTRUCTION(class_name) \
- private: \
-  class_name(); \
-  class_name(const class_name& other); \
-  class_name& operator=(const class_name& other);
-
 #define SINGLETON(class_name) \
  public: \
+  class_name(const class_name& other) = delete; \
+  class_name& operator=(const class_name& other) = delete; \
+  \
   static class_name& getInstance() { \
     static class_name instance; \
     return instance; \
   } \
  private: \
-  class_name(); \
-  class_name(const class_name& other); \
-  class_name& operator=(const class_name& other);
+  class_name();
 
 #endif  // MACROS_H
